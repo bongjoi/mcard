9b3aeb992e468446bf758c1f24355da22f851346
Kang Bongjo <bong@mangocloud.io>
2024-01-26 10:10:08 +0900

    공통 컴포넌트 구현 - Button

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import Text from '@shared/Text';
+import Button from '@shared/Button';
 
 function App() {
   return (
@@ -19,6 +20,22 @@ function App() {
       <Text typography="t5">t5</Text>
       <Text typography="t6">t6</Text>
       <Text typography="t7">t7</Text>
+
+      <div style={{ height: 10, width: '100%', backgroundColor: '#efefef' }} />
+
+      <Button>클릭해주세요</Button>
+      <Button color="success">클릭해주세요</Button>
+      <Button color="error">클릭해주세요</Button>
+      <Button color="success" weak={true}>
+        클릭해주세요
+      </Button>
+      <Button color="error" weak={true}>
+        클릭해주세요
+      </Button>
+      <Button full={true}>클릭해주세요</Button>
+      <Button full={true} disabled={true}>
+        클릭해주세요
+      </Button>
     </div>
   );
 }
