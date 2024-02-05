d3f2b5214761ba9ef84caad8085a2b78282a659e
Kang Bongjo <bong@mangocloud.io>
2024-02-05 15:54:13 +0900

    카드 신청 구현 - 예외처리

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -1,20 +1,53 @@
 import { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
+import FullPageLoader from '@shared/FullPageLoader';
+
 import Apply from '@components/apply';
 import useApplyCardMutation from '@components/apply/hooks/useApplyCardMutation';
 import usePollApplyStatus from '@components/apply/hooks/usePollApplyStatus';
 import { updateApplyCard } from '@remote/apply';
 import { APPLY_STATUS } from '@models/apply';
 import useUser from '@hooks/auth/useUser';
+import useAppliedCard from '@components/apply/hooks/useAppliedCard';
+import { useAlertContext } from '@contexts/AlertContext';
 
 function ApplyPage() {
   const navigate = useNavigate();
+
+  const { open } = useAlertContext();
 
   const [readyToPoll, setReadyToPoll] = useState(false);
 
   const user = useUser();
   const { id } = useParams() as { id: string };
+
+  const { data } = useAppliedCard({
+    userId: user?.uid as string,
+    cardId: id,
+    options: {
+      onSuccess: (applied) => {
+        if (applied === null || applied === undefined) {
+          return;
+        }
+
+        if (applied.status === APPLY_STATUS.COMPLETE) {
+          open({
+            title: '이미 발급이 완료된 카드입니다.',
+            onButtonClick: () => {
+              window.history.back();
+            }
+          });
+
+          return;
+        }
+
+        setReadyToPoll(true);
+      },
+      onError: () => {},
+      suspense: true
+    }
+  });
 
   usePollApplyStatus({
     onSuccess: async () => {
@@ -55,9 +88,16 @@ function ApplyPage() {
     }
   });
 
-  // TODO: 개선
+  if (
+    data !== null &&
+    data !== undefined &&
+    data.status === APPLY_STATUS.COMPLETE
+  ) {
+    return null;
+  }
+
   if (readyToPoll || isLoading) {
-    return <div>Loading...</div>;
+    return <FullPageLoader message="카드를 신청중입니다" />;
   }
 
   return <Apply onSubmit={mutate} />;
